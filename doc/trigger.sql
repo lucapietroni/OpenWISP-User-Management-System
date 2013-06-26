171f6394d49caa8ef53dbfd974e14c9027e96c6e
nitin777 <nitin.barai777@gmail.com>
2013-06-27 00:22:24 +0530

    trigger updated

--- doc/trigger.sql
+++ doc/trigger.sql
@@ -3,17 +3,18 @@ DROP TRIGGER IF EXISTS trig_total_surfing_time;
 DELIMITER $$
 
 CREATE
-    TRIGGER trig_total_surfing_time BEFORE UPDATE ON radacct 
+    TRIGGER trig_total_surfing_time BEFORE INSERT ON radacct 
     FOR EACH ROW
     BEGIN
         SELECT TotalSurfingTime INTO @session_surf_time FROM radacct WHERE UserName = NEW.UserName AND TotalSurfingTime != 0 AND is_surf = 1 ORDER BY RadAcctId DESC LIMIT 1;
-    	IF NOT (NEW.AcctStopTime <=> OLD.AcctStopTime) THEN
-    		SELECT TIMESTAMPDIFF(SECOND, NEW.AcctStartTime, NEW.AcctStopTime) INTO @total_last_seconds FROM radacct WHERE RadAcctId = NEW.RadAcctId LIMIT 1;
-    		IF @session_surf_time THEN
-    			SET NEW.TotalSurfingTime = @total_last_seconds + @session_surf_time;
-    		ELSE
-    			SET NEW.TotalSurfingTime = @total_last_seconds;
-    		END IF;	
-    	END IF;
+    	
+		SELECT TIMESTAMPDIFF(SECOND, NEW.AcctStartTime, NEW.AcctStopTime) INTO @total_last_seconds FROM radacct WHERE RadAcctId = NEW.RadAcctId LIMIT 1;
+		
+		IF @session_surf_time THEN
+			SET NEW.TotalSurfingTime = TIMESTAMPDIFF(SECOND, NEW.AcctStartTime, NEW.AcctStopTime) + @session_surf_time;
+		ELSE
+			SET NEW.TotalSurfingTime = TIMESTAMPDIFF(SECOND, NEW.AcctStartTime, NEW.AcctStopTime);
+		END IF;	
+    	
     END;
 $$
